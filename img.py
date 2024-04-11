e3701707527ab9a813f6835e2f7a018e3c390cb6
Surya Vinay <143257388+20R11A0507@users.noreply.github.com>
2024-04-11 16:56:51 +0530

    Update img.py

--- img.py
+++ img.py
@@ -4,13 +4,13 @@ cpt = 0
 maxFrames = 100
 
 count=0
-cap=cv2.VideoCapture('2wheelertraffic.webm')
+cap=cv2.VideoCapture('2wheelerTrafficWOHelmet.webm')
 while cpt < maxFrames:
     ret, frame = cap.read()
     frame=cv2.resize(frame,(1080,500))
     time.sleep(0.01)
     cv2.imshow("test window", frame)
-    cv2.imwrite(r"C:\Users\surya\Downloads\yolov8helmetdetection-main\yolov8helmetdetection-main\images\helmet_%d.jpg" %cpt, frame)
+    cv2.imwrite(r"path\to\folder\images\helmet_%d.jpg" %cpt, frame)
     cpt += 1
     if cv2.waitKey(5)&0xFF==27:
         break
